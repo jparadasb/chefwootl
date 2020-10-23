24a79943227967a89a4fb8db030890d2be67137d
Jose Paradas <jparadas.b@gmail.com>
2020-10-23 11:29:13 -0300

    finished timeline

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,14 @@ import React from "react"
 import { PageProps, Link, graphql } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
+import SEO from "../components/helpers/seo"
+import { totalmem } from "os"
+import { title } from "process"
 
-type Articles = {
+import "./index.scss"
+import { format } from "prettier"
+
+type Article = {
   node: {
     Content: string
     Title: string
@@ -28,20 +33,63 @@ type Articles = {
 
 type DataProps = {
   allRestApiTimelines: {
-    edges: Array<Articles>
+    edges: {
+      edges: Array<Article>
+    }
   }
 }
 
 const Timeline: React.FC<PageProps<DataProps>> = ({
-  data: { allRestApiTimelines: edges },
+  data: { allRestApiTimelines: {edges: edges} },
   path,
 }) => {
   return (
-    <Layout>
+    <Layout type="clean">
       <SEO title="Using TypeScript" />
-      <h1>Timeline</h1>
-      <Link to="/">Go back to the homepage</Link>
+      <ul className="timeline">
+        {renderTimeRow(edges)}
+      </ul>
     </Layout>
+  )
+}
+
+const renderTimeRow = (rows: Array<Article>): Array<JSX.Element> => rows.map((edge: Article) => <TimeRow key={edge.node.id} {...edge} />)
+
+const dateFormatOptions = {
+  year: 'numeric',
+  month: 'numeric',
+  day: 'numeric'
+};
+
+const formatDate = (date: String): String => {
+  return new Intl.DateTimeFormat('en', dateFormatOptions).format(new Date(date))
+}
+
+
+const TimeRow: React.FC<Article> = ({
+  node: {
+    Content: content, Title: title, created_at, id, image_src, updated_at, updated_by: {
+      firstname: first_name, lastname: last_name
+    }
+  },
+}) => {
+  return (
+    <li key={`li_${id}`} className="timeline__item">
+      <div className="timeline__content arrow arrow-down">
+        <h3>
+          {title}
+        </h3>
+        <p>
+          {content}
+        </p>
+        <aside>
+          {formatDate(created_at)}
+        </aside>
+      </div>
+      <div className="timeline__data-point">
+        <img src="https://picsum.photos/200" alt={title} />
+      </div>
+    </li>
   )
 }
 
